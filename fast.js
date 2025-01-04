323d57c7d3caa8c87d62fa6cba1353a06838461f
jayprakash2002 <143065634+jayprakash2002@users.noreply.github.com>
2025-01-04 21:31:34 +0530

    Add files via upload

---
+++ fast.js
@@ -0,0 +1,10 @@
+//let msg ="ILovecoding";
+//let msg ="   Hello   ";
+//let newmsg=msg.trim().toUpperCase();
+//console.log(newmsg);
+//let msg ="ilovecoding";
+//let msg = "appancollage";
+//let students=["aman","shardha","vikram"];
+for(let i=1; i<=5;i+1){
+    console.log(i);
+}
